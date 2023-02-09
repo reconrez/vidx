318e26b589a8bad55ae55bcc7c76cf5ca3c03483
reconrez <reconrez@gmail.com>
2023-02-09 09:48:50 +0530

    addition of files

---
+++ src/app/shared/components/controlbox/controlbox.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-controlbox',
+  templateUrl: './controlbox.component.html',
+  styleUrls: ['./controlbox.component.scss']
+})
+export class ControlboxComponent {
+
+}
---
+++ src/app/shared/components/time-controls/time-controls.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-time-controls',
+  templateUrl: './time-controls.component.html',
+  styleUrls: ['./time-controls.component.scss']
+})
+export class TimeControlsComponent {
+
+}
